35b7c5785e136242f2dabaa06691e486ac0c996f
吴健泳 <3332189176@qq.com>
2018-06-26 10:31:06 +0800

    fetch

--- src/App.js
+++ src/App.js
@@ -2,8 +2,18 @@ import React, { Component } from 'react';
 import { Button } from 'antd-mobile';
 import logo from './logo.svg';
 import './App.css';
+import fetch from 'unfetch'
 
 class App extends Component {
+  async search(){
+    console.log(111);
+    const ret = await fetch(`http://192.168.2.115:3001/`, {
+        method: 'GET',
+    });
+    console.log(ret);
+    const r = await ret.text();
+    console.log(r);
+  }
   render() {
     return (
       <div className="App">
@@ -14,7 +24,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <Button>Start</Button> 
+        <Button style={{margin:5}} onClick={this.search}>Start</Button> 
       </div>
     );
   }
