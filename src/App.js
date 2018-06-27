0387123e193b5c6ff14484bb2afec89c8bda0a58
吴健泳 <3332189176@qq.com>
2018-06-27 18:29:18 +0800

    a

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
 import React, { Component } from 'react';
-import { Button } from 'antd-mobile';
+import { Button, Toast } from 'antd-mobile';
 import logo from './logo.svg';
 import './App.css';
 import fetch from 'unfetch'
+import * as config from '../config.json';
 
+var getP = function(n, hrefstr) {
+  var pos, parastr, para, tempstr;
+  pos = hrefstr.indexOf("?");
+  parastr = hrefstr.substring(pos + 1);
+  para = parastr.split("&");
+  tempstr = "";
+  for (var i = 0; i < para.length; i++) {
+      tempstr = para[i];
+      pos = tempstr.indexOf("=");
+      if (tempstr.substring(0, pos).toLowerCase() === n.toLowerCase()) {
+          return tempstr.substring(pos + 1);
+      }
+  }
+  return '';
+};
 class App extends Component {
   async componentDidMount(){
-    alert(window.location.href);
-    const ret = await fetch(`http://192.168.2.115:3001/getToken`, {
+    const href = window.location.href;
+    const code = getP('code',href);
+    const state = getP('state',href);
+    Toast.info(code, 1);
+    const ret = await fetch(config.reqUrl+`/getAccess_token?code=${code}&state=${state}`, {
       method: 'GET',
     });
+    console.log(ret);
     const r = await ret.json();
     console.log(r);
     
   }
   async search(){
-    console.log(111);
-    const ret = await fetch(`http://192.168.2.115:3001/`, {
+    const ret = await fetch(config.reqUrl+`?a=1&b=2`, {
         method: 'GET',
     });
     console.log(ret);
