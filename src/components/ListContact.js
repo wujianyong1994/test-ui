1271b4894767d7599119ae1dda9d05adc2a42b0e
吴健泳 <3332189176@qq.com>
2018-07-14 11:24:44 +0800

    a

--- src/components/ListContact.js
+++ src/components/ListContact.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {findDOMNode} from 'react-dom';
 import { Button, NavBar, TabBar, Icon, Popover, List, InputItem, ListView, Accordion } from 'antd-mobile';
 import * as config from '../../config.json';
+import { observer } from 'mobx-react'
 import fetch from '../fetch';
 function MyBody(props) {
     return (
@@ -12,11 +13,17 @@ function MyBody(props) {
     );
   }
 let pageIndex = 0;
+@observer
 export default class ListContact extends Component {
     constructor(props){
         super(props);
+        const getSectionData = (dataBlob, sectionID) => dataBlob[sectionID];
+        const getRowData = (dataBlob, sectionID, rowID) => dataBlob[rowID];
         const dataSource = new ListView.DataSource({
-        rowHasChanged: (row1, row2) => row1 !== row2,
+            // getRowData,
+            // getSectionHeaderData: getSectionData,
+            rowHasChanged: (row1, row2) => row1 !== row2,
+            sectionHeaderHasChanged: (s1, s2) => s1 !== s2,
         });
 
         this.state = {
@@ -55,7 +62,7 @@ export default class ListContact extends Component {
         console.log(this.rData);
     }
     async onChange(groupId){
-        console.log(this.state);
+        console.log(this);
         
         if (!this.state.group[groupId]) {
             const ret = await fetch(config.reqUrl + `/listGroupDetail?groupId=${groupId}`, {
@@ -80,6 +87,8 @@ export default class ListContact extends Component {
         const r = await ret.json();
         if (r.success) {
             this.rData = this.rData.concat(r.data);
+            // let dataArray = Object.assign({}, this.rData)
+            // console.log(dataArray)
             this.setState({
                 dataSource: this.state.dataSource.cloneWithRows(this.rData),
                 isLoading: false
@@ -87,11 +96,12 @@ export default class ListContact extends Component {
         }
         console.log(this.rData);
     }
-    row = (rowData, sectionID, rowID) => {
+    row(rowData, sectionID, rowID) {
         const obj = rowData;
         console.log(obj,sectionID,rowID);
         return (
         <div key={rowID} style={{ padding: '0px 0px' }}>
+            {this.state.group[obj.id]?this.state.group[obj.id].length:0}
             <Accordion accordion  className="my-accordion" onChange={this.onChange.bind(this,obj.id)}>
                 <Accordion.Panel header={obj.name}>
                     <List className="my-list">
@@ -104,36 +114,19 @@ export default class ListContact extends Component {
         </div>
       );
     };
-
+    separator = (sectionID, rowID) => (
+        <div
+          key={`${sectionID}-${rowID}`}
+          style={{
+            backgroundColor: '#F5F5F9',
+            height: 8,
+            borderTop: '1px solid #ECECED',
+            borderBottom: '1px solid #ECECED',
+          }}
+        />
+    );
     render() {
-        const separator = (sectionID, rowID) => (
-          <div
-            key={`${sectionID}-${rowID}`}
-            style={{
-              backgroundColor: '#F5F5F9',
-              height: 8,
-              borderTop: '1px solid #ECECED',
-              borderBottom: '1px solid #ECECED',
-            }}
-          />
-        );
-        const row = (rowData, sectionID, rowID) => {
-            const obj = rowData;
-            console.log(obj,sectionID,rowID);
-            return (
-            <div key={rowID} style={{ padding: '0px 0px' }}>
-                <Accordion accordion  className="my-accordion" onChange={this.onChange.bind(this,obj.id)}>
-                    <Accordion.Panel header={obj.name}>
-                        <List className="my-list">
-                        {this.state.group[obj.id] ? this.state.group[obj.id].map((item,index) => (
-                            <List.Item key={index}>{item.name} <a href={'tel:'+item.mobile}>{item.mobile}</a></List.Item>
-                        )): ''}
-                        </List>
-                    </Accordion.Panel>
-                </Accordion>
-            </div>
-          );
-        };
+        
         return (
             <div>
             {/* <span>我的通讯组</span> */}
@@ -145,7 +138,7 @@ export default class ListContact extends Component {
               {this.state.isLoading ? 'Loading...' : 'Loaded'}
             </div>)}
             renderRow={this.row.bind(this)}
-            renderSeparator={separator}
+            renderSeparator={this.separator}
             className="am-list"
             pageSize={10}
             style={{
